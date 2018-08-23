d9154f3c07201d3837b3551765db8ffa1e33ff5f
nagy135 <viktor.nagy1995@gmail.com>
2018-08-23 14:09:22 +0200

    remove, list

--- notes.py
+++ notes.py
@@ -1,10 +1,12 @@
+#!/bin/python
+
 import subprocess
 import argparse
 import os
 import time
 import getpass
 
-def add():
+def db_add():
     name = input('Name of the note: ')
     subnotes = []
     time_stamp = time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
@@ -37,8 +39,7 @@ def add():
             db.write(subnote + ' 0 \n')
         db.write('\n')
 
-def db_list():
-    print('Parsing db records...')
+def db_list(note_id=None):
     home_folder = os.path.expanduser("~")
     directory = home_folder + '/.config/pynotes/'
     with open(directory + 'pynotes_db','r') as db:
@@ -71,32 +72,63 @@ def db_list():
                     data_notes[order][2].append(line)
             elif line == 'notes:':
                 notes = True
-    print_db_list(data_notes)
+    if note_id is not None:
+        for i, note in enumerate(data_notes):
+            if i != note_id:
+                data_notes[i] = []
+    return data_notes
 
 def print_db_list(data):
     for note_id, note in enumerate(data):
+        if note == []:
+            continue
         name = note[0]
         timestamp = note[1]
         subnotes = note[2]
-        longest_subnote = max([len(x) for x in note[2]])
+        try:
+            longest_subnote = max([len(x) for x in note[2]])
+        except ValueError:
+            longest_subnote = 0
         longest = max(len(name), len(timestamp), longest_subnote)
 
-        print('#' * (longest + 4))
-        print('# #' + str(note_id) + ' ' * (longest - (len(str(note_id))) - 1) + ' #')
+        print(' +' + '-' * (longest + 2) + '+')
+        print(' | #' + str(note_id) + ' ' * (longest - (len(str(note_id))) - 1) + ' |')
         left_padding = (longest - len(name)) // 2
         right_padding = (longest - len(name) - left_padding)
-        print('# ' + '-' * left_padding + name + '-' * right_padding + ' #')
-        print('# ' + timestamp + ' ' * (longest - len(timestamp) ) + ' #')
-        print('#_' + '_' * longest + '_#')
-        for subnote in note[2]:
+        print(' |-' + '-' * left_padding + name + '-' * right_padding + '-|')
+        print(' | ' + timestamp + ' ' * (longest - len(timestamp) ) + ' |')
+        if len(subnotes) > 0:
+            print(' |=' + '=' * longest + '=|')
+        for subnote in subnotes:
             subnote_name = ' '.join(subnote.split()[:-1])
             subnote_val = int(subnote.split()[-1])
             checked = ' '
             if subnote_val:
                 checked = '+'
-            print('# ' + subnote_name + ' ' * (longest - len(subnote_name) ) + checked + '#')
-        print('#' * (longest + 4))
+            print(' | ' + subnote_name + ' ' * (longest - len(subnote_name) ) + checked + '|')
+        print(' +' + '-' * (longest + 2) + '+')
         print()
+
+def db_remove(note_id):
+    home_folder = os.path.expanduser("~")
+    directory = home_folder + '/.config/pynotes/'
+    if not os.path.exists(directory):
+        print('Use init first !')
+        return
+    notes = db_list()
+    del notes[note_id]
+    with open(directory + 'pynotes_db','r') as db:
+        lines = db.readlines()[:5]
+    with open(directory + 'pynotes_db','w') as db:
+        for line in lines:
+            db.write(line)
+        for note in notes:
+            db.write(note[0] + '\n')
+            db.write(note[1] + '\n')
+            for subnote in note[2]:
+                db.write(subnote + '\n')
+            db.write('\n')
+
 
 def initialize():
     print('Initializing...')
@@ -122,16 +154,22 @@ if __name__ == '__main__':
     rows, cols = proc.stdout.read().strip().decode("utf-8").split()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("command", help="command to run")
-    # parser.add_argument("-i", help="number of iterations(default = 5)", type=int)
+    parser.add_argument("command", help="command to run", choices=['init', 'add', 'list', 'remove'])
+    parser.add_argument("-id", help="id of note show (use with list)", type=int)
     # parser.add_argument("-a", help="file is in absolute path format", action="store_true")
     # parser.add_argument("-o", help="name of background file(default = file + _bg)")
     args = parser.parse_args()
     if args.command == 'init':
         initialize()
     elif args.command == 'add':
-        add()
+        db_add()
     elif args.command == 'list':
-        db_list()
+        data_notes = db_list(args.id)
+        print_db_list(data_notes)
+    elif args.command == 'remove':
+        if args.id is not None:
+            db_remove(args.id)
+        else:
+            print('Specify note id, try "list" command')
     else:
         print('Unrecognized command, try --help')
